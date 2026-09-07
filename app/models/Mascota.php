582a4e140393653355acae5d21f347090a4d0795
raidenmaister <joniconafe047@gmail.com>
2026-09-06 21:41:14 -0600

    creacion de carpetas y archivos para sanitizar datos de las mascotas

---
+++ app/models/Mascota.php
@@ -0,0 +1,16 @@
+<?php
+
+// Modelo para la entidad Mascota
+
+class Mascota {
+
+    // Funcion para guardar una nueva mascota en la bd
+    public static function guardar($datos) {
+        // (INSERT INTO mascotas...)
+    }
+
+    // Funcion para obtener todas las mascotas
+    public static function obtenerTodas() {
+        // SELECT * FROM mascotas
+    }
+}
---
+++ app/utils/MascotaValidator.php
@@ -0,0 +1,8 @@
+<?php
+
+// Clase para validar los datos del formulario de mascotas
+// opcional, se pueden dejar las funciones de validacion en el controlador o moverlas aqui
+
+class MascotaValidator {
+    // aqui irian funciones para validar los datos de la mascota
+}
