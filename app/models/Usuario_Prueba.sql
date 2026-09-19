e93791c7f99a405f758eefb3b1b77138bdd3b283
AYALA-008 <trais.garcia@gmail.com>
2026-09-19 12:14:13 -0600

    Agregar usuario de prueba a la base de datos

---
+++ app/models/Usuario_Prueba.sql
@@ -0,0 +1,10 @@
+INSERT INTO usuarios (usuario, contrasena, empleado_asociado, rol, permisos, estado) 
+VALUES (
+    'admin', 
+    '$2y$10$UpcXlKZ.WcliFbPhhAk4vOxdfukl6Kwnx.ESh3ABt2tIXz/upFvKO', 
+    'Administrador General', 
+    'admin', 
+    'todos', 
+    'activo'
+);
+-- Insertar un usuario de prueba a la base de datos(Usuario: admin, Contraseña: password123)
